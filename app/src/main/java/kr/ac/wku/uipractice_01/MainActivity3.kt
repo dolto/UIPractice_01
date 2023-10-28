1e3c9a506c440137b8d87c214f28ea660e7fa9f3
dolto <hdw810@naver.com>
2023-10-28 16:38:20 +0900

    when 써보기

--- app/src/main/java/kr/ac/wku/uipractice_01/MainActivity3.kt
+++ app/src/main/java/kr/ac/wku/uipractice_01/MainActivity3.kt
@@ -15,9 +15,33 @@ class MainActivity3 : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main3)
         binding.btnLog.setOnClickListener {
             Log.d("mainView", "이건 테스트 로그입니다.");
+            Log.e("mainView", "로그 e모드");
+            Log.wtf("mainView", "로그 왓더뻑 모드")
         }
         binding.btnToast.setOnClickListener {
             Toast.makeText(this, "토스트 클릭됨", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnVariable.setOnClickListener {
+            var myName: String
+            myName = "변경전"
+            myName = "변경후"
+            Toast.makeText(this, myName, Toast.LENGTH_SHORT).show()
+        }
+        binding.btnCondition.setOnClickListener {
+            val userAge = 25;
+            when (userAge) {
+                in 20..24 -> {
+                    Toast.makeText(this, "이십대 초반입니다", Toast.LENGTH_SHORT).show()
+                }
+                in 25..27 -> {
+                    Toast.makeText(this, "이십대 중반입니다.", Toast.LENGTH_SHORT).show()
+                }
+                in 28..30 -> {
+                    Toast.makeText(this, "이십대 후반입니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                else -> {Toast.makeText(this, "뭐야.", Toast.LENGTH_SHORT).show()}
+            }
         }
     }
 }
